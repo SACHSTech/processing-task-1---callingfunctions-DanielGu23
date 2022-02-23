bf291fc4c50392db82e372562951140232379bb7
Daniel Gu <99457610+DanielGu23@users.noreply.github.com>
2022-02-23 03:56:18 +0000

    Added comments

--- Sketch.java
+++ Sketch.java
@@ -7,7 +7,7 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
   public void settings() {
-	// put your size call here
+	// Size of sketchpad
     size(400, 400);
   }
 
@@ -16,22 +16,25 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
-	
-	// sample code, delete this stuff
+    // Draw the head
     stroke(255);
     ellipse(200, 200, 220, 220);
-    
+
+    // Draw the ears
     stroke(255);
     line(150, 125, 60, 50); 
     line(120, 160, 60, 50);
     line(250, 125, 340, 50); 
     line(280, 160, 340, 50);
-    
+
+    // Draw the eyes
     stroke(0);
     rect(125, 150, 40, 30);
     ellipse(145, 165, 10, 10);
     rect(235, 150, 40, 30);  
     ellipse(255, 165, 10, 10);
+
+    // Draw the nose and mouth
     triangle(175, 225, 225, 225, 200, 175);
     noFill();
     arc(250, 225, 100, 90, HALF_PI, PI);
